39403758c7fa8db7fc962b2640c2ba92667ca2a6
Can Kockan <ckockan@sfu.ca>
2016-07-27 17:54:55 -0700

    Updating the command line options

--- main.cpp
+++ main.cpp
@@ -26,14 +26,14 @@ int main( int argc, char** argv)
 	
 	desc.add_options()
 		( "help,h", "Print help message.")
-		( "errorRate,e", value<double>(), "Error rate for the sequencing platform used.")
-		( "minDepth,m", value<int>(), "Minimum Read Depth required for high confidence in a call.")
-		( "leftStrandBias,l", value<double>(), "Lower limit for the strand bias value interval to be used in assessing the confidence of a call.")
-		( "rightStrandBias,r", value<double>(), "Upper limit for the strand bias value interval to be used in assessing the confidence of a call.")
-		( "readEndFraction,f", value<double>(), "Average position of the called base on the reads supporting the call as a fraction. End values such as 0.01 as useful for filtering read end artifacts.")
-		( "QScoreCutoff,q", value<int>(), "Cutoff value for the qScore assigned to each call by the Poisson model used.")
-		( "tumorDirectoryPath,t", value< vector<string> >(), "Specifies directory for the input files.")
-		( "outputDirectoryPath,o", value< vector<string> >(), "Specifies directory for the output files.");
+		( "error-rate,e", value<double>(), "Error rate for the sequencing platform used.")
+		( "min-depth,m", value<int>(), "Minimum Read Depth required for high confidence in a call.")
+		( "left-strand-bias,l", value<double>(), "Lower limit for the strand bias value interval to be used in assessing the confidence of a call.")
+		( "right-strand-bias,r", value<double>(), "Upper limit for the strand bias value interval to be used in assessing the confidence of a call.")
+		( "read-end-fraction,f", value<double>(), "Average position of the called base on the reads supporting the call as a fraction. End values such as 0.01 as useful for filtering read end artifacts.")
+		( "qscore-cutoff,q", value<int>(), "Cutoff value for the qScore assigned to each call by the Poisson model used.")
+		( "tumor-directory-path,t", value< vector<string> >(), "Specifies directory for the input files.")
+		( "output-directory-path,o", value< vector<string> >(), "Specifies directory for the output files.");
 	
 	// Parse the command line, catching and displaying any parser errors
 	variables_map vm;
@@ -62,24 +62,24 @@ int main( int argc, char** argv)
 	if(vm.count("help"))
 	{
 		cout << desc << endl;
-		cout << "  --tumorDirectoryPath and --outputDirectoryPath must be specified" << endl << endl;
-		std::cout << "  Usage: ./sinvict -e [errorRate] -m [minDepth] -l [leftStrandBias] -r [rightStrandBias] ";
-		std::cout << "-f [readEndFraction] -q [QScoreCutoff] -t <tumorDirectoryPath> -o <outputDirectoryPath>" << std::endl;
+		cout << "  --tumor-directory-path and --output-directory-path must be specified" << endl << endl;
+		std::cout << "  Usage: ./sinvict -e [error-rate] -m [min-depth] -l [left-strand-bias] -r [right-strand-bias] ";
+		std::cout << "-f [read-end-fraction] -q [qscore-cutoff] -t <tumor-directory-path> -o <output-directory-path>" << std::endl;
 		exit(1);
 	}
 
-	if(vm.count("tumorDirectoryPath"))
+	if(vm.count("tumor-directory-path"))
 	{
-		vector<string> inputPath = vm["tumorDirectoryPath"].as< vector<string> >();
-		cout << "--tumorDirectoryPath specified with value = " << inputPath[0] << endl;
+		vector<string> inputPath = vm["tumor-directory-path"].as< vector<string> >();
+		cout << "--tumor-directory-path specified with value = " << inputPath[0] << endl;
 		tumorDirectoryPath.assign(inputPath[0]);
 		tumorDirectorySpecified = true;
 	}
 
-	if(vm.count("outputDirectoryPath"))
+	if(vm.count("output-directory-path"))
 	{
-		vector<string> outputPath = vm["outputDirectoryPath"].as< vector<string> >();
-		cout << "--outputDirectoryPath specified with value = " << outputPath[0] << endl;
+		vector<string> outputPath = vm["output-directory-path"].as< vector<string> >();
+		cout << "--output-directory-path specified with value = " << outputPath[0] << endl;
 		outputDirectoryPath.assign(outputPath[0]);
 		outputDirectorySpecified = true;
 	}
@@ -87,71 +87,71 @@ int main( int argc, char** argv)
 	if( !tumorDirectorySpecified || !outputDirectorySpecified)
 	{
 		cout << desc << endl;
-		cout << "  --tumorDirectoryPath and --outputDirectoryPath must be specified" << endl << endl;
-		std::cout << "  Usage: ./sinvict -e [errorRate] -m [minDepth] -l [leftStrandBias] -r [rightStrandBias] ";
-		std::cout << "-f [readEndFraction] -q [QScoreCutoff] -t <tumorDirectoryPath> -o <outputDirectoryPath>" << std::endl;
+		cout << "  --tumor-directory-path and --output-directory-path must be specified" << endl << endl;
+		std::cout << "  Usage: ./sinvict -e [error-rate] -m [min-depth] -l [left-strand-bias] -r [right-strand-bias] ";
+		std::cout << "-f [read-end-fraction] -q [qscore-cutoff] -t <tumor-directory-path> -o <output-directory-path>" << std::endl;
 		exit( 1);
 	}
 	else
 	{
-		if(vm.count("errorRate"))
+		if(vm.count("error-rate"))
 		{
-			errorRate = vm["errorRate"].as<double>();
-			cout << "--errorRate set to: " << errorRate << endl;
+			errorRate = vm["error-rate"].as<double>();
+			cout << "--error-rate set to: " << errorRate << endl;
 		}
 		else
 		{
-			cout << "--errorRate not set, using default value: " << errorRate << endl;
+			cout << "--error-rate not set, using default value: " << errorRate << endl;
 		}
 
-		if(vm.count("minDepth"))
+		if(vm.count("min-depth"))
 		{
-			minDepth = vm["minDepth"].as<int>();
-			cout << "--minDepth set to: " << minDepth << endl;
+			minDepth = vm["min-depth"].as<int>();
+			cout << "--min-depth set to: " << minDepth << endl;
 		}
 		else
 		{
-			cout << "--minDepth not set, using default value: " << minDepth << endl;
+			cout << "--min-depth not set, using default value: " << minDepth << endl;
 		}
 
-		if(vm.count("leftStrandBias"))
+		if(vm.count("left-strand-bias"))
 		{
-			leftStrandBias = vm["leftStrandBias"].as<double>();
-			cout << "--leftStrandBias set to: " << leftStrandBias << endl;
+			leftStrandBias = vm["left-strand-bias"].as<double>();
+			cout << "--left-strand-bias set to: " << leftStrandBias << endl;
 		}
 		else
 		{
-			cout << "--leftStrandBias not set, using default value: " << leftStrandBias << endl;
+			cout << "--left-strand-bias not set, using default value: " << leftStrandBias << endl;
 		}
 
-		if(vm.count("rightStrandBias"))
+		if(vm.count("right-strand-bias"))
 		{
-			rightStrandBias = vm["rightStrandBias"].as<double>();
-			cout << "--rightStrandBias set to: " << rightStrandBias << endl;
+			rightStrandBias = vm["right-strand-bias"].as<double>();
+			cout << "--right-strand-bias set to: " << rightStrandBias << endl;
 		}
 		else
 		{
-			cout << "--rightStrandBias not set, using default value: " << rightStrandBias << endl;
+			cout << "--right-strand-bias not set, using default value: " << rightStrandBias << endl;
 		}
 
-		if(vm.count("readEndFraction"))
+		if(vm.count("read-end-fraction"))
 		{
-			readEndFraction = vm["readEndFraction"].as<double>();
-			cout << "--readEndFraction set to: " << readEndFraction << endl;
+			readEndFraction = vm["read-end-fraction"].as<double>();
+			cout << "--read-end-fraction set to: " << readEndFraction << endl;
 		}
 		else
 		{
-			cout << "--readEndFraction not set, using default value: " << readEndFraction << endl;
+			cout << "--read-end-fraction not set, using default value: " << readEndFraction << endl;
 		}
 
-		if(vm.count("QScoreCutoff"))
+		if(vm.count("qscore-cutoff"))
 		{
-			qScoreCutoff = vm["QScoreCutoff"].as<int>();
-			cout << "--QScoreCutoff set to: " << qScoreCutoff << endl;
+			qScoreCutoff = vm["qscore-cutoff"].as<int>();
+			cout << "--qscore-cutoff set to: " << qScoreCutoff << endl;
 		}
 		else
 		{
-			cout << "--QScoreCutoff not set, using default value: " << qScoreCutoff << endl;
+			cout << "--qscore-cutoff not set, using default value: " << qScoreCutoff << endl;
 		}
 
 		Caller caller( errorRate, minDepth, leftStrandBias, rightStrandBias, readEndFraction, qScoreCutoff, tumorDirectoryPath.c_str(), outputDirectoryPath.c_str());
