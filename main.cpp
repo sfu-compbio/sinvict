bd6572f59ccaf9d2311f1c7e6652a55f8bd0d593
Can Kockan <ckockan@iu.edu>
2017-03-05 19:42:29 -0500

    Added checks for input parameter types

--- main.cpp
+++ main.cpp
@@ -1,43 +1,22 @@
 #include <iostream>
-#include <string>
 #include <fstream>
-#include <exception>
+#include <string>
 #include <cstdlib>
 #include "argh.h"
 #include "Caller.h"
 
-using namespace std;
+void printHelp();
 
 int main(int argc, char** argv)
 {
 	// Initialize the "argh" command-line parser
 	argh::parser cmdl(argc, argv);
 
-	// Help message
-	std::string desc = "";
-	desc = desc + "\t--help or -h : Print help message.\n";
-	desc = desc + "\t--error-rate or -e : Error rate for the sequencing platform used.\n";
-	desc = desc + "\t--min-depth or -m : Minimum Read Depth required for high confidence in a call.\n";
-	desc = desc + "\t--left-strand-bias or -l : Lower limit for the strand bias value interval to be used in assessing the confidence of a call.\n";
-	desc = desc + "\t--right-strand-bias or -r : Upper limit for the strand bias value interval to be used in assessing the confidence of a call.\n";
-	desc = desc + "\t--read-end-fraction or -f : Average position of the called base on the reads supporting the call as a fraction.";
-	desc = desc + "End values such as 0.01 as useful for filtering read end artifacts.\n";
-	desc = desc + "\t--qscore-cutoff or -q : Cutoff value for the qScore assigned to each call by the Poisson model used.\n";
-	desc = desc + "\t--tumor-directory-path or -t : Specifies directory for the input files.\n";
-	desc = desc + "\t--output-directory-path or -o : Specifies directory for the output files.\n";
-
+	// If the help flag is set, print help message and exit
 	if(cmdl["--help"] || cmdl["-h"])
 	{
-		std::cout << ("SiNVICT: Ultra Sensitive Detection of Single Nucleotide Variants and Indels in Circulating Tumour DNA.");
-		std::cout << std::endl << std::endl;
-		std::cout << ("Allowed arguments:");
-		std::cout << std::endl;
-		std::cout << desc;
-
-		std::cout << "\t--tumor-directory-path and --output-directory-path must be specified" << std::endl << std::endl;
-		std::cout << "\tUsage: ./sinvict -e=[error-rate] -m=[min-depth] -l=[left-strand-bias] -r=[right-strand-bias] ";
-		std::cout << "-f=[read-end-fraction] -q=[qscore-cutoff] -t=<tumor-directory-path> -o=<output-directory-path>" << std::endl;
-		exit(0);
+		printHelp();
+		return 0;
 	}
 
 	// Default parameters
@@ -47,73 +26,96 @@ int main(int argc, char** argv)
 	double rightStrandBias = 0.7;
 	double readEndFraction = 0.01;
 	int qScoreCutoff = 95;
-	string tumorDirectoryPath;
-	string outputDirectoryPath;
+	std::string tumorDirectoryPath;
+	std::string outputDirectoryPath;
 	bool tumorDirectorySpecified = false;
 	bool outputDirectorySpecified = false;
 
 	// Now go through all command arguments to overwrite default arguments when applicable
 	for(auto& param : cmdl.params())
 	{
-		// std::cout << "\t" << param.first << " : " << param.second << std::endl;
 		if(param.first == "tumor-directory-path" || param.first == "t")
 		{
-			std::string inputPath = param.second;
-			tumorDirectoryPath.assign(inputPath);
+			if(!(cmdl(param.first) >> tumorDirectoryPath))
+			{
+				std::cerr << "Invalid value entered for tumor-directory-path. Must be string." << std::endl;
+				printHelp();
+				return 0;
+			}
 			tumorDirectorySpecified = true;
 		}
-
-		if(param.first == "output-directory-path" || param.first == "o")
+		else if(param.first == "output-directory-path" || param.first == "o")
 		{
-			std::string outputPath = param.second;
-			outputDirectoryPath.assign(outputPath);
+			if(!(cmdl(param.first) >> outputDirectoryPath))
+			{
+				std::cerr << "Invalid value entered for output-directory-path. Must be string." << std::endl;
+				printHelp();
+				return 0;
+			}
 			outputDirectorySpecified = true;
 		}
-
-		if(param.first == "error-rate" || param.first == "e")
+		else if(param.first == "error-rate" || param.first == "e")
 		{
-			cmdl(param.first) >> errorRate;
+			if(!(cmdl(param.first) >> errorRate))
+			{
+				std::cerr << "Invalid value entered for error-rate. Must be double >= 0.0" << std::endl;
+				printHelp();
+				return 0;
+			}
 		}
-
-		if(param.first == "min-depth" || param.first == "m")
+		else if(param.first == "min-depth" || param.first == "m")
 		{
-			cmdl(param.first) >> minDepth;
+			if(!(cmdl(param.first) >> minDepth))
+			{
+				std::cerr << "Invalid value entered for min-depth. Must be integer > 0." << std::endl;
+				printHelp();
+				return 0;
+			}
 		}
-
-		if(param.first == "left-strand-bias" || param.first == "l")
+		else if(param.first == "left-strand-bias" || param.first == "l")
 		{
-			cmdl(param.first) >> leftStrandBias;
+			if(!(cmdl(param.first) >> leftStrandBias))
+			{
+				std::cerr << "Invalid value entered for left-strand-bias. Must be double >= 0.0" << std::endl;
+				printHelp();
+				return 0;
+			}
 		}
-
-		if(param.first == "right-strand-bias" || param.first == "r")
+		else if(param.first == "right-strand-bias" || param.first == "r")
 		{
-			cmdl(param.first) >> rightStrandBias;
+			if(!(cmdl(param.first) >> rightStrandBias))
+			{
+				std::cerr << "Invalid value entered for right-strand-bias. Must be double >= 0.0" << std::endl;
+				printHelp();
+				return 0;
+			}
 		}
-
-		if(param.first == "read-end-fraction" || param.first == "f")
+		else if(param.first == "read-end-fraction" || param.first == "f")
 		{
-			cmdl(param.first) >> readEndFraction;
+			if(!(cmdl(param.first) >> readEndFraction))
+			{
+				std::cerr << "Invalid value entered for read-end-fraction. Must be double >= 0.0" << std::endl;
+				printHelp();
+				return 0;
+			}
 		}
-
-		if(param.first == "qscore-cutoff" || param.first == "q")
+		else if(param.first == "qscore-cutoff" || param.first == "q")
 		{
-			cmdl(param.first) >> qScoreCutoff;
+			if(!(cmdl(param.first) >> qScoreCutoff))
+			{
+				std::cerr << "Invalid value entered for qscore-cutoff. Must be integer > 0" << std::endl;
+				printHelp();
+				return 0;
+			}
 		}
 	}
 
 	// Input and Output directories MUST be specified
+	// If not, print help message and exit
 	if(!tumorDirectorySpecified || !outputDirectorySpecified)
 	{
-		std::cout << ("SiNVICT: Ultra Sensitive Detection of Single Nucleotide Variants and Indels in Circulating Tumour DNA.");
-		std::cout << std::endl << std::endl;
-		std::cout << ("Allowed arguments:");
-		std::cout << std::endl;
-		std::cout << desc;
-
-		std::cout << "\t--tumor-directory-path and --output-directory-path must be specified" << std::endl << std::endl;
-		std::cout << "\tUsage: ./sinvict -e=[error-rate] -m=[min-depth] -l=[left-strand-bias] -r=[right-strand-bias] ";
-		std::cout << "-f=[read-end-fraction] -q=[qscore-cutoff] -t=<tumor-directory-path> -o=<output-directory-path>" << std::endl;
-		exit(0);
+		printHelp();
+		return 0;
 	}
 
 	// Print final set of parameters
@@ -130,4 +132,32 @@ int main(int argc, char** argv)
 	caller.callLocationsMixture();
 
 	return 0;
+}
+
+void printHelp()
+{
+	// Help message body
+	std::string desc = "";
+	desc = desc + "\t--help or -h : Print help message.\n";
+	desc = desc + "\t--error-rate or -e : Error rate for the sequencing platform used.\n";
+	desc = desc + "\t--min-depth or -m : Minimum Read Depth required for high confidence in a call.\n";
+	desc = desc + "\t--left-strand-bias or -l : Lower limit for the strand bias value interval to be used in assessing the confidence of a call.\n";
+	desc = desc + "\t--right-strand-bias or -r : Upper limit for the strand bias value interval to be used in assessing the confidence of a call.\n";
+	desc = desc + "\t--read-end-fraction or -f : Average position of the called base on the reads supporting the call as a fraction.";
+	desc = desc + "End values such as 0.01 as useful for filtering read end artifacts.\n";
+	desc = desc + "\t--qscore-cutoff or -q : Cutoff value for the qScore assigned to each call by the Poisson model used.\n";
+	desc = desc + "\t--tumor-directory-path or -t : Specifies directory for the input files.\n";
+	desc = desc + "\t--output-directory-path or -o : Specifies directory for the output files.\n";
+
+	// Print help message
+	std::cout << ("\nSiNVICT: Ultra Sensitive Detection of Single Nucleotide Variants and Indels in Circulating Tumour DNA.");
+	std::cout << std::endl << std::endl;
+	std::cout << ("Allowed arguments:");
+	std::cout << std::endl;
+	std::cout << desc;
+	std::cout << std::endl;
+
+	std::cout << "\t--tumor-directory-path and --output-directory-path must be specified\n" << std::endl;
+	std::cout << "\tUsage: ./sinvict -e=[error-rate] -m=[min-depth] -l=[left-strand-bias] -r=[right-strand-bias] ";
+	std::cout << "-f=[read-end-fraction] -q=[qscore-cutoff] -t=<tumor-directory-path> -o=<output-directory-path>\n" << std::endl;
 }
